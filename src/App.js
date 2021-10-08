bd90af06a4e1d58b5275ba5361534375840f7f17
hai.nguyen <haingdc@gmail.com>
2021-10-09 04:40:36 +0700

    refactor navbar, create api, add search bar, pagination to listing screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Component, useEffect, useMemo, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import { useObservable } from 'rxjs-hooks'
 import { ajax } from 'rxjs/ajax'
-import { fromFetch } from 'rxjs/fetch'
-import { map } from 'rxjs/operators';
+// TODO: remove
+import { useObservable } from 'rxjs-hooks'
 import './containers/universities/pages/universities-listing/universities-listing.scss';
 import './containers/authenticate/pages/sign-in/sign-in.scss';
 import './containers/authenticate/pages/sign-up/sign-up.scss';
@@ -91,6 +94,10 @@ export class SignUp extends Component {
   }
 }
 
+function UniversityListContainer(props) {
+
+}
+
 function UniversityItem(props) {
   const { name, country, domain } = props;
   return (
@@ -102,25 +109,111 @@ function UniversityItem(props) {
   );
 }
 
+const UniversityApi = {
+  search(options) {
+    const paramTuples = [
+      ['name', options.uniName],
+      ['country', options.country],
+    ];
+    const queryString = paramTuples
+    .filter(([_, fieldVal]) => fieldVal)
+    .map(([fieldName, fieldVal]) => `${fieldName}=${fieldVal}`)
+    .join('&');
+
+    return ajax.getJSON(`http://universities.hipolabs.com/search?${queryString}`);
+  }
+};
+
+function UIPagination(props) {
+  const { className = '' } = props;
+  return (
+    <Pagination className={className}>
+      <Pagination.Prev />
+      <Pagination.Item active>{1}</Pagination.Item>
+      <Pagination.Item >{2}</Pagination.Item>
+      <Pagination.Item >{3}</Pagination.Item>
+      <Pagination.Item >{4}</Pagination.Item>
+      <Pagination.Next />
+    </Pagination>
+  )
+}
+
 
 function ListingPage() {
   const [data, setData] = useState([]);
+  const [keywords, setKeywords] = useState('');
+  const [submitValues, setSubmitValues] = useState({ uniName: '' });
 
   useEffect(() => {
-    const subscription = ajax.getJSON('http://universities.hipolabs.com/search?name=middle')
+    if (!submitValues.uniName) return () => {};
+    const subscription = UniversityApi.search(submitValues)
       .subscribe(data => setData(data));
 
-      return () => subscription.unsubscribe();
-  }, [])
+    return () => subscription.unsubscribe();
+  }, [submitValues])
 
   return (
-    <div className="universities-listing">
-      { data.map(n => {
-        const { name, country } = n;
-        const domain = n.domains?.length ? n.domains[0] : undefined;
-        return <UniversityItem key={name} name={name} domain={domain} country={country} />;
-      }) }
-    </div>
+    <Container className="universities-listing">
+      <Container className="universities-listing__searchbar">
+        <Row>
+          <Col xs={4}>
+            <Form>
+              <Form.Control
+                type="text"
+                placeholder="Enter a name of university"
+                value={keywords}
+                onChange={evt => setKeywords(evt.target.value)}
+              />
+            </Form>
+          </Col>
+          <Col xs={2}>
+            <Button variant="outline-primary" onClick={() => setSubmitValues({ uniName: keywords })}>Search</Button>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="universities-listing__list">
+        <Row>
+          <Col xs={4}>School Name</Col>
+          <Col xs={4}>Domain</Col>
+          <Col xs={4}>Country</Col>
+        </Row>
+        { data.map((n, index) => {
+          const { name, country } = n;
+          const domain = n.domains?.length ? n.domains[0] : undefined;
+          // return <UniversityItem key={name} name={name} domain={domain} country={country} />;
+          return (
+            <Row key={index}>
+              <Col xs={4}>{ name }</Col>
+              <Col xs={4}><a href={domain}>{ domain }</a></Col>
+              <Col xs={4}>{ country }</Col>
+            </Row>
+          )
+        }) }
+      </Container>
+      <Container>
+        <UIPagination className="justify-content-center" />
+      </Container>
+    </Container>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <div className="container">
+        <Link className="navbar-brand" to={"/"}>Home</Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-in"}>Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
@@ -128,23 +221,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* TODO: create NavBar component */}
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/"}>Home</Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>Login</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
+        <NavBar />
         <Switch>
           <Route exact path='/' component={ListingPage} />
           <Route path="/sign-in" component={SignIn} />
