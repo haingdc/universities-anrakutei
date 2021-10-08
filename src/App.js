e5649fd8fd2413d0def47abb56fe5206b1fe59bf
hai.nguyen <haingdc@gmail.com>
2021-10-08 17:42:20 +0700

    add node-sass package, create style files for each screens

--- src/App.js
+++ src/App.js
@@ -8,35 +8,44 @@ import { useObservable } from 'rxjs-hooks'
 import { ajax } from 'rxjs/ajax'
 import { fromFetch } from 'rxjs/fetch'
 import { map } from 'rxjs/operators';
+import './containers/universities/pages/universities-listing/universities-listing.scss';
+import './containers/authenticate/pages/sign-in/sign-in.scss';
+import './containers/authenticate/pages/sign-up/sign-up.scss';
 // TODO: remove package: rxjs-hooks
 
-export class Login extends Component {
+export class SignIn extends Component {
   render() {
     return (
-      <Form>
-        <h3>Sign In</h3>
+      <div className="sign-in">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Form>
+              <h3>Sign In</h3>
 
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" className="form-control" placeholder="Enter email"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" className="form-control" placeholder="Enter email"></Form.Control>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" className="form-control" placeholder="Enter password"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" className="form-control" placeholder="Enter password"></Form.Control>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="remember">
-          <div className="custom-control custom-checkbox">
-            <Form.Check inline label="Remember me" type="checkbox" className="custom-control-input" id="customCheck1"></Form.Check>
+              <Form.Group className="mb-3" controlId="remember">
+                <div className="custom-control custom-checkbox">
+                  <Form.Check inline label="Remember me" type="checkbox" className="custom-control-input" id="customCheck1"></Form.Check>
+                </div>
+              </Form.Group>
+
+              <Button type="submit" variant="primary" className="btn btn-primary btn-block">Submit</Button>
+              <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+              </p>
+            </Form>
           </div>
-        </Form.Group>
-
-        <Button type="submit" variant="primary" className="btn btn-primary btn-block">Submit</Button>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </Form>
+        </div>
+      </div>
     );
   }
 }
@@ -44,39 +53,57 @@ export class Login extends Component {
 export class SignUp extends Component {
   render() {
     return (
-      <Form>
-        <h3>Sign Up</h3>
+      <div className="sign-in">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Form>
+              <h3>Sign Up</h3>
 
-        <Form.Group className="mb-3">
-          <Form.Label>First name</Form.Label>
-          <Form.Control type="text" className="form-control" placeholder="First name"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>First name</Form.Label>
+                <Form.Control type="text" className="form-control" placeholder="First name"></Form.Control>
+              </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control type="text" className="form-control" placeholder="Last name"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control type="text" className="form-control" placeholder="Last name"></Form.Control>
+              </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" className="form-control" placeholder="Enter email"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" className="form-control" placeholder="Enter email"></Form.Control>
+              </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" className="form-control" placeholder="Enter password"></Form.Control>
-        </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" className="form-control" placeholder="Enter password"></Form.Control>
+              </Form.Group>
 
-        <Button type="submit" variant="primary" className="btn btn-primary btn-block">Sign Up</Button>
-        <p className="forgot-password text-right">
-          Already registered <a href="#">sign in?</a>
-        </p>
-      </Form>
+              <Button type="submit" variant="primary" className="btn btn-primary btn-block">Sign Up</Button>
+              <p className="forgot-password text-right">
+                Already registered <a href="#">sign in?</a>
+              </p>
+            </Form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-function Listing() {
+function UniversityItem(props) {
+  const { name, country, domain } = props;
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{domain}</div>
+      <div>{country}</div>
+    </div>
+  );
+}
+
+
+function ListingPage() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -87,15 +114,11 @@ function Listing() {
   }, [])
 
   return (
-    <div>
+    <div className="universities-listing">
       { data.map(n => {
-        return (
-          <div>
-            <div>{n.name}</div>
-            <div>{n.country}</div>
-            <div>{n.domains?.length ? n.domains[0] : 'NA'}</div>
-          </div>
-        )
+        const { name, country } = n;
+        const domain = n.domains?.length ? n.domains[0] : undefined;
+        return <UniversityItem key={name} name={name} domain={domain} country={country} />;
       }) }
     </div>
   );
@@ -122,15 +145,11 @@ function App() {
           </div>
         </nav>
 
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Switch>
-              <Route exact path='/' component={Listing} />
-              <Route path="/sign-in" component={Login} />
-              <Route path="/sign-up" component={SignUp} />
-            </Switch>
-          </div>
-        </div>
+        <Switch>
+          <Route exact path='/' component={ListingPage} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
